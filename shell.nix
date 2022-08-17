bebe41a8a0f2c6988cd29fa6af1c7c9b4d717a7c
Gustavo Porto <gustavo@portosanti.com>
2022-08-17 19:11:15 +0100

    chore: create derivation with udev rules for FHS build targets

--- shell.nix
+++ shell.nix
@@ -1,13 +1,21 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  udevRules = ./misc/watchy-usb.rules;
+  watchy-usb-rules = pkgs.stdenv.mkDerivation {
+    name = "watchy-usb-rules";
+    rules = pkgs.writeTextFile {
+      name = "50-watchy-usb-rules.rules";
+      text = builtins.readFile ./misc/watchy-usb.rules;
+    };
+    buildCommand = ''
+      mkdir -p $out/etc/udev/rules.d
+      cp $rules $out/etc/udev/rules.d/50-watchy-usb.rules
+    '';
+  };
   fhs = pkgs.buildFHSUserEnv {
     name = "watchy-usb";
-    targetPkgs = pkgs: (with pkgs; [ platformio avrdude ]);
-    extraInstallCommands = ''
-      install -D ${udevRules} /etc/udev/rules.d/50-watchy-usb.rules
-    '';
+    targetPkgs = pkgs:
+      ((with pkgs; [ udev platformio avrdude ]) ++ [ watchy-usb-rules ]);
     runScript = "bash";
   };
 in fhs.env
