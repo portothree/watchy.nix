4b66d9a0f8c3497dc2b7f70f67e5db1a72b9646d
Gustavo Porto <gustavo@portosanti.com>
2022-08-17 18:24:28 +0100

    feat: add avrdude

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 let udevRules = ./misc/watchy-usb.rules;
 in pkgs.mkShell {
-  buildInputs = with pkgs; [ platformio ];
+  buildInputs = with pkgs; [ platformio avrdude ];
   shellHook = ''
     install -D ${udevRules} /etc/udev/rules.d/50-watchy-usb.rules
   '';
