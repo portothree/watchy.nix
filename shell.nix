b3c232555e74d1c68934b570a8e49efd5215c28c
Gustavo Porto <gustavo@portosanti.com>
2022-08-17 18:47:55 +0100

    chore: create fhs user dev

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
 
-let udevRules = ./misc/watchy-usb.rules;
-in pkgs.mkShell {
-  buildInputs = with pkgs; [ platformio avrdude ];
-  shellHook = ''
-    install -D ${udevRules} /etc/udev/rules.d/50-watchy-usb.rules
-  '';
-}
+let
+  udevRules = ./misc/watchy-usb.rules;
+  fhs = pkgs.buildFHSUserEnv {
+    name = "watchy-usb";
+    targetPkgs = pkgs: (with pkgs; [ platformio avrdude ]);
+    extraInstallCommands = ''
+      install -D ${udevRules} /etc/udev/rules.d/50-watchy-usb.rules
+    '';
+    runScript = "bash";
+  };
+in fhs.env
